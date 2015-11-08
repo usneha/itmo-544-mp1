03b494df1c177123299a5343494e354f103add77
usneha <usneha@hawk.iit.edu>
2015-11-08 16:38:06 -0600

    fixed the chmod file path

--- dbcreate.sh
+++ dbcreate.sh
@@ -13,7 +13,7 @@ sudo apt-get install php5-mysql
 
 # passing the db instances to the setup.php script
 
-chmod 700 /itmo-544-mp1/setup.php
+chmod 700 ./itmo-544-mp1/setup.php
 
 php ./itmo-544-mp1/setup.php $dbresult
 
