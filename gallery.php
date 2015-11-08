4355408dd91c4d0f8a5b0e9e294ceba1e224a366
usneha <usneha@hawk.iit.edu>
2015-11-08 15:23:40 -0600

    adding gallery and submit pages

---
+++ gallery.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+require 'vendor/autoload.php';
+
+#create RDSclient using the us-west-2 
+$rds = new Aws\Rds\RdsClient([
+    'version' => 'latest',
+    'region'  => 'us-west-2'
+]);
+
+#fetch the DB instance
+$result = $rds->describeDBInstances(['DBInstanceIdentifier' => 'usneha']);
+
+
+#get the end point to the instance
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+    print "============\n". $endpoint . "================";
+echo "endpoint is available";
+
+$link = mysqli_connect($endpoint,"username","password","usnehadb");
+
+?>
+
+<!DOCTYPE html>
+<html><head>
+<!-- adding jquery gallery as per graduate requirement -->
+
+</head>
+<body>
+<?php 
+if (!$link)
+{
+die("connection failed". mysqli_connect_error());
+}
+
+else
+{
+if(isset($_POST['email'])){
+$useremail = $_POST['email'];
+$sqlstat= "SELECT * FROM items WHERE Email='$useremail'";
+}
+else
+{
+$sqlstat= "SELECT * FROM items";
+}
+
+$output = mysqli_query($link, $sqlstat);
+
+$imgPath = array();
+print "Result set order...\n";
+
+if (mysqli_num_rows($output) > 0) {
+    while($row = mysqli_fetch_assoc($output)) {
+        
+        $imgPath[$row["JpgFileName"]] = $row["RawS3URL"];
+        echo "id: " . $row["ID"]."- RawS3URL" . $row["RawS3URL"]. "<br>";
+    }
+} 
+else {
+    echo "no rows in the output";
+}
+
+$link->close();
+}
+?>
+<ul class="gallery">
+  <?php foreach ($imgPath as $key => $value) {
+  ?>
+  <li>	
+  <a href="<?php echo $value ?>"> <img src="<?php echo $value ?>"></img><?php echo $key ?></a>
+  </li>
+  <?php }?>
+</ul>
+
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+</html>
---
+++ submit.php
@@ -0,0 +1,134 @@
+<?php
+session_start();
+require 'vendor/autoload.php';
+
+echo "Inside submit  page";
+
+if(!empty($_POST)){
+	echo $_POST['email'];
+	echo $_POST['phone'];
+}
+else {
+
+	echo "No data has been posted";
+}
+print_r ($_POST);
+
+echo $_FILES['userfile'];
+
+print_r ($_FILES);
+
+if (isset ($_FILES['userfile'])){
+	$uploaddir = '/tmp/';
+	$uploadfile = $uploaddir. basename($_FILES['userfile']['name']);
+
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    print "File is valid, and was successfully uploaded.\n";
+} else {
+    print "Couldnt upload file\n";
+
+}
+
+print 'Here is some more debugging info:';
+print_r($_FILES);
+$s3=new Aws\S3\S3Client([
+    'version' => 'latest',
+    'region'  => 'us-west-2'
+]);
+$bucket = uniqid("usnehaS3-", false);
+print "Creating bucket named {$bucket}\n";
+$result = $s3->createBucket([
+    'ACL' => 'public-read',
+    'Bucket' => $bucket
+]);
+$result = $s3->waitUntil('BucketExists',array('Bucket' => $bucket));
+echo "bucket creation done";
+$result = $s3->putObject([
+    'ACL' => 'public-read',
+    'Bucket' => $bucket,
+   'Key' => "uploads".$uploadfile,
+'ContentType' => $_FILES['userfile']['type'],
+    'Body'   => fopen($uploadfile, 'r+')
+]);  
+$url = $result['ObjectURL'];
+echo $url;
+echo "s3 file uploaded";
+$rds = new Aws\Rds\RdsClient([
+    'version' => 'latest',
+    'region'  => 'us-west-2'
+]);
+$result = $rds->describeDBInstances(['DBInstanceIdentifier' => 'usneha']);
+
+echo "fetching result from describe db instance";
+
+print_r($result);
+
+$endpoint = $result['DBInstances'][0]['Endpoint']['Address'];
+    print "============\n". $endpoint . "================";
+echo "endpoint is available";
+
+$link = mysqli_connect($endpoint,"usneha","password","usnehadb") or die("Error " . mysqli_error($link));
+
+print_r($link);
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+echo "Connected to RDS";
+
+$sql_insert = "INSERT INTO items (UName,Email,Phone,RawS3Url,FinalS3Url,JpgFileName,status,Issubscribed) VALUES (?,?,?,?,?,?,?,?)";
+
+if (!($stmt = $link->prepare($sql_insert))) {
+    echo "Prepare failed: (" . $link->errno . ") " . $link->error;
+}
+else
+{
+echo "statement was success";
+}
+$uname = $_POST['username'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$s3rawurl = $url; 
+$s3finishedurl = "none";
+$filename = basename($_FILES['userfile']['name']);
+$status =0;
+$issubscribed=0;
+$stmt->bind_param("ssssssii",$uname,$email,$phone,$s3rawurl,$s3finishedurl,$filename,$status,$issubscribed);
+if (!$stmt->execute()) {
+    print "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+printf("%d Row inserted.\n", $stmt->affected_rows);
+$stmt->close();
+$sql1 = "SELECT ID, JpgFileName, RawS3URL FROM items ";
+$output = mysqli_query($link, $sql1);
+$imgPath = array();
+print "Result set order...\n";
+if (mysqli_num_rows($output) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($output)) {
+        //this will append the path of images to an array
+        $imgPath[$row["JpgFileName"]] = $row["RawS3URL"];
+        echo "id: " . $row["ID"]."- RawS3URL" . $row["RawS3URL"]. "<br>";
+    }
+} 
+else {
+    echo "Did not get any output";
+}
+$link->close();
+
+
+function redirect()
+{
+   echo "inside redirect";
+   header('Location: gallery.php', true, 303);
+   die();
+}
+redirect();
+}
+else
+{
+echo "file invalid";
+}
+?>
+
