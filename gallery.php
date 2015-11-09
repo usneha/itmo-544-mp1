12b74a97918fbe595775b3e1b15ff94754fc82f1
usneha <usneha@hawk.iit.edu>
2015-11-09 08:43:38 -0600

    changed gallery

--- gallery.php
+++ gallery.php
@@ -16,8 +16,31 @@
 }
 
 
-</style>
+/*.gallery-item{
+	
+  enabled: false, // set to true to enable gallery
 
+  preload: [0,2], // read about this option in next Lazy-loading section
+
+  navigateByImgClick: true,
+
+  arrowMarkup: '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+
+  tPrev: 'Previous (Left arrow key)', // title for left button
+  tNext: 'Next (Right arrow key)', // title for right button
+  tCounter: '<span class="mfp-counter">%curr% of %total%</span>' // markup of counter
+}*/
+
+</style>
+<script>
+$('.gallery-item').magnificPopup({
+  type: 'image',
+  gallery:{
+    enabled:true
+  }
+});
+
+</script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 
 <script src="https://raw.githubusercontent.com/usneha/itmo-544-mp1/master/js/jquery.magnific-popup.js"></script>
@@ -28,7 +51,7 @@
 </head>
 <body>
 <h3> Welcome to your Gallery! </h3>
-<div class="magnific-gallery">
+<div class="gallery-item">
 <?php
 session_start();
 #$email = $_POST["useremail"];
