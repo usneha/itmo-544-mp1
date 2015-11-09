bb35cabda37dc4a0077a53a8318c2edc553abed9
usneha <usneha@hawk.iit.edu>
2015-11-09 00:06:38 -0600

    adding gallery

--- gallery.php
+++ gallery.php
@@ -5,8 +5,8 @@
 
 <?php
 session_start();
-$email = $_POST["useremail"];
-echo $email;
+#$email = $_POST["useremail"];
+#echo $email;
 require 'vendor/autoload.php';
 
 
@@ -50,13 +50,13 @@ if (mysqli_connect_errno()) {
     exit();
 }
 //below line is unsafe - $email is not checked for SQL injection -- don't do this in real life or use an ORM instead
-$link->real_query("SELECT * FROM items WHERE email = '$email'");
+$link->real_query("SELECT * FROM items");
 //$link->real_query("SELECT * FROM items");
 $res = $link->use_result();
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()) {
     echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
-echo $row['id'] . "Email: " . $row['email'];
+#echo $row['id'] . "Email: " . $row['email'];
 }
 $link->close();
 ?>
